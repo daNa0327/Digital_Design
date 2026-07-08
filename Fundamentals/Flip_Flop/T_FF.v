399ca8ee535318e9867864cd8d1f47f8293abca5
daNa0327 <jjy3067410@gmail.com>
2026-07-08 12:17:32 +0900

    T Flip Flop module

--- Fundamentals/Flip_Flop/T_FF.v
+++ Fundamentals/Flip_Flop/T_FF.v
@@ -1,3 +1,39 @@
-module T_FF ();
+module T_FF (
+    output reg Q,
+    input T,
+    input CK
+);
 
+initial begin // 초기 Q를 0으로 설정
+    Q = 0;
+end
+
+always @(posedge CK) begin
+    Q <= T ? ~Q : Q;
+end
+
+endmodule
+
+module Top;
+
+    reg T, CK;
+    wire Q;
+
+    always #1 CK = ~CK;
+
+    T_FF m1(Q, T, CK);
+
+    initial begin
+        CK = 0;
+        T = 0;
+
+        $monitor("CK = %b / T = %b / Q = %b", CK, T, Q);
+
+        #3 T = 1;
+        #3 T = 0;
+        #3 T = 1;
+        #3 T = 0;
+        
+        #1 $finish;
+    end
 endmodule
