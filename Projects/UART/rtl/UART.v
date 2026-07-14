1f3485ca37db2ef81866230899cc5c7116e74dad
daNa0327 <jjy3067410@gmail.com>
2026-07-14 16:51:10 +0900

    UART Top file Update

--- Projects/UART/rtl/UART.v
+++ Projects/UART/rtl/UART.v
@@ -1,7 +1,44 @@
-module UART (
-    input ck,
-    input reset
+module UART(
+    input        ck,
+    input        reset,
 
+    input        tx_start,
+    input  [7:0] tx_data,
+    output       tx,
+    output       tx_busy,
+
+    input        rx,
+    output [7:0] rx_data,
+    output       data_valid
 );
+    wire baud_tick;
+
+    Baud_Rate_Generator b1(
+        .ck(ck),
+        .reset(reset),
+
+        .baud_tick(baud_tick)
+    );
+
+    UART_TX t1(
+        .ck(ck),
+        .reset(reset),
+        .baud_tick(baud_tick),
+        .tx_start(tx_start),
+        .tx_data(tx_data),
+
+        .tx(tx),
+        .busy(tx_busy)
+    );
+
+    UART_RX r1(
+        .ck(ck),
+        .reset(reset),
+        .baud_tick(baud_tick),
+        .rx(rx),
+
+        .data_valid(data_valid),
+        .rx_data(rx_data)
+    );
 
 endmodule
